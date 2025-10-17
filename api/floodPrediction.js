1753a27ba0e5d7f47630ad49ffb79da9d77dcca6
SudaisFarooq <sudaisfarooq8@gmail.com>
2025-10-18 02:30:55 +0530

    change

--- api/floodPrediction.js
+++ api/floodPrediction.js
@@ -1,11 +1,14 @@
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Recreate __dirname and __filename in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default function handler(req, res) {
   const filePath = path.join(__dirname, '..', 'public', 'floodPrediction.html');
-  res.sendFile(filePath);
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(fileContent);
 }
